785f521eee2409c55bab571229d9bd276e6ad604
Erik Nyquist <erik.nyquist@uc.se>
2023-12-14 22:09:42 +0100

    Day 9, exercise 2

--- day9/src/main/java/se/nyquist/Day9.java
+++ day9/src/main/java/se/nyquist/Day9.java
@@ -24,6 +24,7 @@ public class Day9 {
             if (stream != null) {
                 var lines = new BufferedReader(new InputStreamReader(stream)).lines().filter(not(String::isEmpty)).toList();
                 execise1(lines);
+                execise2(lines);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -34,7 +35,11 @@ public class Day9 {
         System.out.println("Exercise 1: " + lines.stream().flatMap(Day9::history).map(BigInteger::valueOf).reduce(BigInteger::add).orElse(BigInteger.ZERO));
     }
 
-    private static Stream<Long> history(String line) {
+    private static void execise2(List<String> lines) {
+        System.out.println("Exercise 2: " + lines.stream().flatMap(Day9::history2).map(BigInteger::valueOf).reduce(BigInteger::add).orElse(BigInteger.ZERO));
+    }
+
+    private static ArrayList<List<Long>> populate(String line) {
         var numbers = Stream.of(line.split("\\s+")).map(Long::parseLong).toList();
         var diffs = computeDiffs(numbers);
         var history = new ArrayList<List<Long>>();
@@ -44,6 +49,15 @@ public class Day9 {
             diffs = computeDiffs(diffs);
             history.add(diffs);
         }
+        return history;
+    }
+
+    private static List<Long> computeDiffs(List<Long> numbers) {
+        return IntStream.range(0, numbers.size() - 1).mapToLong(i -> numbers.get(i + 1) - numbers.get(i)).boxed().toList();
+    }
+
+    private static Stream<Long> history(String line) {
+        var history = populate(line);
         int i = history.size()-1;
         ArrayList<Long> next = new ArrayList<>(history.get(i));
         next.add(0L);
@@ -58,8 +72,21 @@ public class Day9 {
         return Stream.of(next.getLast());
     }
 
-    private static List<Long> computeDiffs(List<Long> numbers) {
-        return IntStream.range(0, numbers.size() - 1).mapToLong(i -> numbers.get(i + 1) - numbers.get(i)).boxed().toList();
+
+    private static Stream<Long> history2(String line) {
+        var history = populate(line);
+        int i = history.size()-1;
+        ArrayList<Long> next = new ArrayList<>(history.get(i));
+        next.add(0, 0L);
+        while(i > 0) {
+            var prev = next;
+            next = new ArrayList<>(history.get(i-1));
+            var extra = next.getFirst() - prev.getFirst();
+            next.add(0, extra);
+            i--;
+        }
+        // System.out.println(next.getLast());
+        return Stream.of(next.getFirst());
     }
 
 }
