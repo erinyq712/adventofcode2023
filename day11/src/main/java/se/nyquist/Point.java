5cd73cef8272cbeba271a91fa876f702a3166381
Erik Nyquist <erik.nyquist@uc.se>
2024-01-01 19:30:39 +0100

    Added compare

--- day11/src/main/java/se/nyquist/Point.java
+++ day11/src/main/java/se/nyquist/Point.java
@@ -1,6 +1,11 @@
 package se.nyquist;
 
 public record Point(int x, int y) {
+    public static int compare(Point p1, Point p2) {
+        var yCompare = Long.compare(p1.y,p2.y);
+        return yCompare == 0 ? Long.compare(p1.x,p2.x) : yCompare;
+    }
+
     public Point add(Point delta) {
         return new Point(this.x + delta.x, this.y + delta.y);
     }
